e2f3e882084373b19074d7ae4619b111c54483db
olindenaus <madorgon@gmail.com>
2019-11-20 17:07:46 +0100

    added React.lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,37 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import { connect } from 'react-redux';
-
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+
 import Layout from './components/UI/Layout/Layout';
 import WorldMapView from './containers/WorldMap/WorldMapView';
-import Trainings from './containers/Trainings/Trainings';
-import TimeKeeper from './containers/Timekeeper/Timekeeper';
-import SongOfADay from './containers/SongOfADay/SongOfADay';
-import Auth from './auth/Auth';
-import AdminPanel from './containers/AdminPanel/AdminPanel';
-import LogsHistory from './containers/LogsHistory/LogsHistory'
+import Spinner from './components/UI/Spinner/Spinner';
 import './App.css';
 
-// const TimeKeeper = React.lazy(()=> {
-//   return import('./components/Timekeeper/Timekeeper');
-// });
+const Trainings = lazy(() => { return import('./containers/Trainings/Trainings') });
+const TimeKeeper = lazy(() => { return import('./containers/Timekeeper/Timekeeper'); });
+const Auth = lazy(() => { return import('./auth/Auth'); });
+const AdminPanel = lazy(() => { return import('./containers/AdminPanel/AdminPanel'); });
+const LogsHistory = lazy(() => { return import('./containers/LogsHistory/LogsHistory'); });
+const SongOfADay = lazy(() => { return import('./containers/SongOfADay/SongOfADay'); });
 
 const App = (props: any) => {
 
   const adminPanel = props.isAuthenticated ? <Route path="/admin" component={AdminPanel} /> : null;
 
   const routes = (
-    <Switch>
-      <Route path='/map' component={WorldMapView} />
-      <Route path='/trainings' component={Trainings} />
-      <Route path='/timekeeper' component={TimeKeeper} />
-      <Route path='/song' component={SongOfADay} />
-      <Route path='/login' component={Auth} />
-      {adminPanel}
-      <Route path='/logs' component={LogsHistory} />
-      <Route path='/' component={WorldMapView} />
-      <Redirect to='/' />
-    </Switch>
+    <React.Suspense fallback={<Spinner />}>
+      <Switch>
+        <Route path='/map' component={WorldMapView} />
+        <Route path='/trainings' component={Trainings} />
+        <Route path='/timekeeper' component={TimeKeeper} />
+        <Route path='/song' component={SongOfADay} />
+        <Route path='/login' component={Auth} />
+        {adminPanel}
+        <Route path='/logs' component={LogsHistory} />
+        <Route path='/' component={WorldMapView} />
+        <Redirect to='/' />
+      </Switch>
+    </React.Suspense>
   );
   return (
     <Layout >
